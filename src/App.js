ac97e23306045c3da4e817301baf7acea3917701
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2022-08-07 17:44:45 +0200

    event added

--- src/App.js
+++ src/App.js
@@ -6,33 +6,17 @@ import {Scene1} from './Loader1.js'
 import { AmbientLight } from 'three'
 import { ModelD } from './Loader2.js'
 
-export function Box(props) {
-  // This reference will give us direct access to the mesh
-  const mesh = useRef()
-  // Set up state for the hovered and active state
-  const [hovered, setHover] = useState(false)
-  const [active, setActive] = useState(false)
-  // Subscribe this component to the render-loop, rotate the mesh every frame
-  useFrame((state, delta) => (mesh.current.rotation.x += 0.01))
-  // Return view, these are regular three.js elements expressed in JSX
-  return (
-    <mesh
-      {...props}
-      ref={mesh}
-      scale={active ? 1.5 : 1}
-      onClick={(event) => setActive(!active)}
-      onPointerOver={(event) => setHover(true)}
-      onPointerOut={(event) => setHover(false)}>
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
-    </mesh>
-  )
-}
+
+
 function App() {
+
+   function show(){
+    console.log('clicked');
+   }
   return (
     <div>
 
-   
+   <button onClick={(event) => show()}>show construction</button>
   <Canvas>
     <OrbitControls />
     
